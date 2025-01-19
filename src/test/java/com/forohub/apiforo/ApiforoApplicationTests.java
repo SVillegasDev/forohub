72d398a6577063542f357561a4799be29cf056b0
svillegas <villegasrojas91@gmail.com>
2025-01-19 12:36:38 -0300

    first commit

---
+++ src/test/java/com/forohub/apiforo/ApiforoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forohub.apiforo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiforoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
